91bf6f9daa366f9744d91cedfae5db1a6cc44c74
adityacandra02 <aditya.candra02@gmail.com>
2025-04-05 17:55:42 +0900

    Add backend and Netlify config
    Add backend functions and Netlify config

--- backend/fetchMarketData.js
+++ backend/fetchMarketData.js
@@ -1,1 +1,16 @@
-// Fetch market data from CMC 
+const fetchTopCoins = require('./utils/coinmarketcap');
+
+exports.handler = async function (event, context) {
+  try {
+    const coins = await fetchTopCoins(10);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ coins })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
